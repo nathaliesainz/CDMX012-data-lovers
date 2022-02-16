2e599b28fb47415a9c5c89f283e3416c26927997
Andrea Urias <urmoandrea@gmail.com>
2022-02-16 17:56:28 -0600

    main.jsComentado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {films} from './data.js';
+ import data from './data/ghibli/ghibli.js';
+ //import studio from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+const filmsScreen = document.getElementById("containerCards");
+
+
+console.log(data,films,filmsScreen);
+
+/***¿cómo vamos a estructurar nuestros datos? // mandarlos al HTML de una forma genérica:
+  añadir un elemento al DOM: parent.appendChild(elemento)
+- crear una variable que capture los cards de las películas
+- crear una función que permita mostrar las peliculas en pantalla (function showFilms)
+- iterar datos // cómo estructurar datos // como crear elementos/objetos en js dependiendo de los datos
+- cascaron
+- resultados esperados: poder mostrar la información de las películas en la interfaz con el título, poster y año */
+ 
